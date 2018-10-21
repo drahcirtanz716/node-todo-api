c438162ccf17a5aca279a3275bff673dfe032639
Drahcir Tanz <drahcir.716@gmail.com>
2018-10-22 04:41:58 +0800

    Fixing Error Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://drahcirtanz:rapscallionz2012@ds149268.mlab.com:49268/todoapp', { useNewUrlParser: true });
+mongoose.connect('mongodb://drahcirtanz:rapscallionz2012@ds149268.mlab.com:49268/todoapp' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
+// mongoose.connect('mongodb://localhost:27017/TodoApp' || 'mongodb://drahcirtanz:rapscallionz2012@ds149268.mlab.com:49268/todoapp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
+// const port = 3000 || process.env.PORT;
 var app = express();
 
 app.use(bodyParser.json());
